74cda33e9cf852997a1f5594ac1b415bd757711a
maliksalem1 <99354681+maliksalem1@users.noreply.github.com>
2022-03-09 22:43:07 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Created by: maliksalem1
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+}
